0366a5f5889ed8baa2682879a3a0a8dafe2773fc
mason-ch01 <mach3337@colorado.edu>
2024-11-04 16:27:29 -0700

    added db views for user stats

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS posts(
     userId INT REFERENCES users(userId) ON DELETE CASCADE,
     songId INT REFERENCES songs(songId),
     playlistId INT REFERENCES playlists(playlistId),
-    likes INT NOT NULL
+    likes INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS comments(
@@ -45,3 +45,32 @@ CREATE TABLE IF NOT EXISTS comments(
     postId INT REFERENCES posts(postId) ON DELETE CASCADE NOT NULL,
     comment TEXT NOT NULL
 );
+
+CREATE VIEW user_follower_count AS
+SELECT u.userId, u.username, COUNT(f.followerId) AS follower_count
+FROM users u
+LEFT JOIN followers f ON u.userId = f.followeeId
+GROUP BY u.userId, u.username;
+
+CREATE VIEW user_following_count AS
+SELECT u.userId, u.username, COUNT(f.followerId) AS following_count
+FROM users u
+LEFT JOIN followers f ON u.userId = f.followerId
+GROUP BY u.userId, u.username;
+
+-- Use this Query for follower and following count
+--COALESCE keeps the query from blowing up. Defaults to 0
+
+-- SELECT 
+--     u.userId,
+--     u.username,
+--     COALESCE(f.follower_count, 0) AS follower_count,
+--     COALESCE(g.following_count, 0) AS following_count
+-- FROM 
+--     users u
+-- LEFT JOIN 
+--     user_follower_count f ON u.userId = f.userId
+-- LEFT JOIN 
+--     user_following_count g ON u.userId = g.userId
+-- WHERE 
+--     u.username = '<username>';
