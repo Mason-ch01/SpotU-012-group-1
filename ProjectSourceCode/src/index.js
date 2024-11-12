3cae679a85706e12970274e9f9785101872f1972
Tawhid Ather <tawher10@gmail.com>
2024-11-11 17:06:23 -0700

    Registration and API Routes

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -166,49 +166,6 @@ app.get('/spotify_callback', async function(req, res) {
     }
 });
 
-app.get('/share', (req, res) => {
-  res.render('pages/share');
-});
-
-  
-    // To-DO: Insert username and hashed password into the 'users' table
-//     db.none(
-//       'INSERT INTO users_db(username, password) VALUES ($1, $2);',
-//       [req.body.username, hash]
-//     )
-//       .then(data=>{
-//         res.status(201).redirect('/login')
-//       })
-//       .catch(err =>{
-//         console.log(err);
-//         res.status(501).redirect('/add_user')
-//       })
-//     );
-
-// app.post('/add_user', async (req, res) => {
-//   const { username, id, name, dob } = req.body;
-
-//   if (!username || !id || !dob) {
-//     return res.status(400).json({ message: 'Invalid input' });
-//   }
-
-//   try {
-//     // Hash the password
-//     // const hash = await bcrypt.hash(password, 10);
-
-//     await db.none(
-//       'INSERT INTO users_db (id, name, dob) VALUES ($1, $2, $3, $4);',
-//       [id, username, name, dob]
-//     );
-
-//     // Respond with success message
-//     res.status(200).json({ message: 'Success' });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: 'Database error' });
-//   }
-// });
-
   
   // authentication
   // const auth = (req, res, next) => {
