7e1b880e03bb5ab350239034234552c4698245ba
Zev Kunianski <zev@Zevs-MacBook-Pro.local>
2024-12-04 17:29:55 -0700

    changes

--- ProjectSourceCode/src/resources/js/scripts.js
+++ ProjectSourceCode/src/resources/js/scripts.js
@@ -35,34 +35,7 @@ async function saveProfilePhoto(pfp) {
     }
 }
 
-// document.getElementById('follow').addEventListener('click', async function () {
-//     const button = this;
-//     const isFollowing = button.getAttribute('data-is-following') === 'true';
-//     const username = document.getElementById('username').innerHTML; // Assumes profile username is displayed here
 
-//     try {
-//         const response = await fetch(`/profile/${username}/follow`, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 isFollowing: !isFollowing, // Toggle the current state
-//             }),
-//         });
-
-//         if (response.ok) {
-//             // Toggle the button text and data attribute based on the new state
-//             button.setAttribute('data-is-following', !isFollowing);
-//             button.innerText = !isFollowing ? 'Unfollow' : 'Follow';
-//         } else {
-//             alert('Failed to update follow state.');
-//         }
-//     } catch (error) {
-//         console.error('Error toggling follow state:', error);
-//         alert('An error occurred while trying to follow/unfollow.');
-//     }
-// });
 
 
 
