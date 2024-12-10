250a3ae5be9acb41c9ab881d0af6a054a7ce88d4
mason-ch01 <mach3337@colorado.edu>
2024-12-09 22:16:14 -0700

    changed host

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -4,10 +4,10 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="<DATABASE_EXTERNAL_URL>"
+PG_URI="postgresql://spotu:UdS08AaaGX4q0SksEHVmxOoBiu7Q3bhX@dpg-ctbsp6rtq21c73dfmqr0-a.oregon-postgres.render.com/spotudb_2y4k"
 
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in "/Users/masonchoi/Documents/School/Fall2024/CSCI3308/SpotU/ProjectSourceCode/src/init_data/create.sql"; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
